237dfe8c951f787da575713b1d53bc0788e27a7a
Agung Maulana <amdev142@gmail.com>
2025-05-27 11:11:22 +0700

    Update telegram_service.ts

--- app/services/telegram_service.ts
+++ app/services/telegram_service.ts
@@ -11,7 +11,7 @@ class TelegramService {
     try {
       await axios.post(url, {
         chat_id: telegramChatId,
-        message_thread_id: 1331,
+        message_thread_id: 1582,
         text: message,
       })
       console.log('Berhasil kirim ke Telegram')
